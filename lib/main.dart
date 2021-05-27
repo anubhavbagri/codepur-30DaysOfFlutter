2b9e53307e7140f973222cdb4fbce2c8b2ac4527
anubhavbagri <anubhav.199302029@muj.manipal.edu>
2021-05-27 20:46:20 +0530

    [+] Day 3

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-
-import 'home_page.dart';
+import 'pages/login_page.dart';
+import 'pages/home_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,22 +9,32 @@ void main() {
 // ignore: use_key_in_widget_constructors
 class MyApp extends StatelessWidget {
   // const MyApp({Key? key}) : super(key: key)
-
   @override
   Widget build(BuildContext context) {
-
-    // double p = 66.39;
-    // bool isMale = true;
-    // num temp = 30.5;
-
-    // var day = "Thursday";
-    // var dayNum = 4;
-    // const pi = 3.14;
-
-    // final
-
+    // bringVegetables(rupees: 50);
+    // bringVegetables(thaila: true);
     return MaterialApp(
-      home: HomePage(),
+      // home: HomePage(),
+      themeMode: ThemeMode.light,
+      // themeMode: ThemeMode.dark,
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      darkTheme:
+          ThemeData(brightness: Brightness.dark, primarySwatch: Colors.red),
+      initialRoute: "/home",
+      routes: {
+        "/": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage(),
+      },
     );
   }
+
+  //default value of rupees is 100 and thaila is false
+  // bringVegetables({
+  //   @required bool? thaila, //supplying thaila's value is compulsory
+  //   int rupees = 100,
+  // }) {
+  //   // Take cycle
+  //   //Go to Sec 16 market
+  // }
 }
---
+++ lib/pages/login_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+// ignore: use_key_in_widget_constructors
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const Material(
+      child: Center(
+        child: Text(
+          "Login Page",
+          style: TextStyle(
+            fontSize: 20,
+            color: Colors.blue,
+            fontWeight: FontWeight.bold,
+          ),
+          // textScaleFactor: 2.0,
+          
+        ),
+      ),
+    );
+  }
+}
