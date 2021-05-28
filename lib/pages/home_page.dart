06a21da6fa40633be7f05fdbba794bbc5b67ca65
anubhavbagri <anubhav.199302029@muj.manipal.edu>
2021-05-29 02:37:58 +0530

    Load & Decode JSON

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,12 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
 import 'package:flutter_application_1/models/catalogue.dart';
 import 'package:flutter_application_1/widgets/drawer.dart';
 import 'package:flutter_application_1/widgets/item_widget.dart';
 
 // ignore: use_key_in_widget_constructors
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   final int days = 30;
+
   final String name = "Anubhav";
+
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    final catalogueJson =
+        await rootBundle.loadString("assets/files/catalogue.json");
+    // print(catalogueJson);
+
+    //decoding json string into another format(map)
+    final decodedData = jsonDecode(catalogueJson);
+    // print(decodedData);
+
+    var productsData = decodedData["products"];
+    // print(productsData);
+    // if we have a map and we want to convert it to string -> jsonEncode()
+  }
 
   @override
   Widget build(BuildContext context) {
