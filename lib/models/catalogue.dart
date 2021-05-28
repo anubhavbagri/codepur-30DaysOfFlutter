bdb5a07a30c56e279d30cb13cfa6d2408d0d23c8
anubhavbagri <anubhav.199302029@muj.manipal.edu>
2021-05-29 03:36:54 +0530

    Making Class from Map

--- lib/models/catalogue.dart
+++ lib/models/catalogue.dart
@@ -26,4 +26,15 @@ class Item {
       required this.price,
       required this.color,
       required this.image});
+
+  factory Item.fromMap(Map<String, dynamic> map) {
+    return Item(
+      id: map["id"],
+      name: map["name"],
+      desc: map["desc"],
+      price: map["price"],
+      color: map["color"],
+      image: map["image"],
+    );
+  }
 }
