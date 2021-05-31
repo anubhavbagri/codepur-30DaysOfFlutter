79a191e35494f2d7c8110572e2b6e5a549060a99
anubhavbagri <anubhav.199302029@muj.manipal.edu>
2021-05-31 05:35:30 +0530

    Removed # from webapp url

--- lib/main.dart
+++ lib/main.dart
@@ -6,8 +6,10 @@ import 'package:velocity_x/velocity_x.dart';
 import 'core/store.dart';
 import 'pages/login_page.dart';
 import 'pages/home_page.dart';
+import 'package:url_strategy/url_strategy.dart';
 
 void main() {
+  setPathUrlStrategy();
   runApp(VxState(
     store: MyStore(),
     // interceptors: [],
